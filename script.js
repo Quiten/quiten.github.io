6759e25f6d56f2dabc989c04c8b4ac2699508381
Quinten de leeuw <quinten.de.leeuw@coderclass.nl>
2019-03-14 09:42:05 +0100

    https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_sidebar_animate

---
+++ script.js
@@ -0,0 +1,6 @@
+function menu_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+function menu_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
