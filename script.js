a1d7a6e2369b7f8a95420669c13eeece6be7c875
Quinten de leeuw <quinten.de.leeuw@coderclass.nl>
2019-03-19 09:56:56 +0100

    small

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function open_menu() {
-  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("sidebar").style.width = "200px";
 }
 function close_menu() {
   document.getElementById("sidebar").style.width = "0";
