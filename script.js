da7573e9382b8784470dc2ff2011709bde3fbb0f
Quiten <quinten.de.leeuw@coderclass.nl>
2020-03-20 15:08:17 +0100

    KING CRIMSON
    Time erasure function has been added to the Bites za dusto

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function open_menu() {
 function close_menu() {
   document.getElementById("sidebar").style.width = "0";
 }
+
+
+function playsong() {
+  document.getElementById("Player").play();
+}
+
+function pausesong() {
+  document.getElementById("Player").pause();
+}
+
+function stopsong() {
+  document.getElementById('Player').setcurrentTime();/**tried also with audio.currentTime here. Didn't worked **/
+  audio.currentTime = 0;
+}
+
+function forwardAudio() {
+  audio = document.getElementById('Player'); /**tried also with audio.currentTime here. Didn't worked **/
+  crim = document.getElementById("Son");
+  crim.play();
+  audio.currentTime += 10.0
+
+}
