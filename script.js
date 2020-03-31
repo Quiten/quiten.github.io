a6f390ddbf786bee5fa3a9c35072ad3b06445438
Quiten <quinten.de.leeuw@coderclass.nl>
2020-03-31 11:36:46 +0200

    ZA WARUDO
    Lazytown

--- script.js
+++ script.js
@@ -10,25 +10,38 @@ function playsong() {
   document.getElementById("Player").play();
 }
 
-function pausesong() {
-  document.getElementById("Player").pause();
+function pausesong2() {
+  myAudio = document.getElementById("Player");
+  d = document.getElementById("Wereld");
+
+  d.play();
+
+  if(myAudio.paused){
+    myAudio.play();
+} else {  
+  myAudio.pause();
+}
 }
 
 function stopsong() {
-  document.getElementById('Player').setcurrentTime();/**tried also with audio.currentTime here. Didn't worked **/
+  audio = document.getElementById('Player');
+  exp = document.getElementById("Gold");
   audio.currentTime = 0;
+  exp.play();
+  audio.play();
 }
 
 function forwardAudio() {
-  audio = document.getElementById('Player'); /**tried also with audio.currentTime here. Didn't worked **/
+  audio = document.getElementById('Player');
   crim = document.getElementById("Son");
   crim.play();
   audio.currentTime += 10.0
 
 }
 
-function resetAudio() {
-  audio = document.getElementById('player');
-  crim = document.getElementById('Son');
-  
+function playbackAudio() {
+  audio = document.getElementById('Player');
+  b = document.getElementById("Bites");
+  b.play();
+  audio.currentTime -= 10.0;
 }
